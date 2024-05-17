35447dc83615d4c9dede1987332226003aa2cf34
MuhammadRizo <muhammadrizosaidumarbekov19@gmail.com>
2024-05-17 13:14:02 +0500

    some

--- tournament/settings.py
+++ tournament/settings.py
@@ -1,6 +1,9 @@
 import os
 from pathlib import Path
+from environs import Env
 
+env = Env()
+env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -11,10 +14,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-237jc4@=m0s9l2=jun5rkwxuccql-7503)6(g+kqlz53^px8h@'
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
